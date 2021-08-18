05d621d608ddc290e53aa7285448859e13260976
Mariam Ammar <59864691+MariamAmmar@users.noreply.github.com>
2021-08-18 20:01:47 +0800

    Add files via upload

--- User.py
+++ User.py
@@ -1,10 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
 
 """
 Mariam Ammar
 Class: CS 521 - Summer 2
 Date: 08/20/21
 Final Project 
-
+Defines the 'User' class which
+with instance attributes name, close contact, 
+birthday, and mood. Includes methods that 
+calculate days till user birthday, allow user to generate
+a madlib, counts the characters and unique words in a journal
+input, 
 """
 
 
@@ -20,21 +30,28 @@ class User():
         self.birthday = birthday
         self.mood = mood
         self.journal_entry = ''
-        
 
     def calculate_days_till_bday(self, original_date, now = datetime.now()):
         delta1 = datetime(now.year, original_date.month, original_date.day)
         delta2 = datetime(now.year+1, original_date.month, original_date.day)
         return ((delta1 if delta1 > now else delta2) - now).days
     
+    
+    def mad_lib(self, n1, n2, adj1, adj2, adj3):
+        return f'''
+        There was an {adj1} {n1} that lived in a {n2}. She was {adj3},
+        lived life without a care, and had very long, {adj2} hair.'''
+        
+    
     def __len__(self):
         return len(self.journal_entry)
     
     def __repr__(self):
-        return f"{self.name} has a mood score of {self.mood}."
+        return f"{self.name} has a mood score of {self.mood}"
     
-    def __write_in_journal(self, file_name = "journal.txt"):
-        self.journal_entry = input("\n Go ahead and let your thoughts flow!\n ")
+    def __write_in_journal(self, text = ' ', file_name = "journal.txt"):
+        text = input("\n Go ahead and let your thoughts flow!\n ")
+        self.journal_entry = text
 
         try:
             new_file = open(file_name, 'a')
@@ -44,7 +61,28 @@ class User():
 
         else:     
             new_file.write(f"\n{datetime.now()}\n" + self.journal_entry)
-            new_file.close() 
-            print(f'''Wow, that was {len(self)} characters. Great work! You entry was saved in '{file_name}' .''')
+            new_file.close()
+            words_lst = set([word for word in self.journal_entry.split()])
+            if len(words_lst) < 2:
+                return        f'''Wow, that was {len(self)} characters and {len(words_lst)} unique word.
+        Great work! You entry was saved in '{file_name}' .'''
+            else: 
+                        return        f'''Wow, that was {len(self)} characters and {len(words_lst)} unique words.
+        Great work! You entry was saved in '{file_name}' .'''
+                  
+          
         
-        
+if __name__ == "__main__":
+    birthday = datetime.now()
+    p1 = User("Mariam","Lorena", birthday, 7)
+    assert p1.calculate_days_till_bday(birthday) == 364
+    n1, n2, adj1, adj2, adj3 = "chair","tree","yellow","angry","hungry"
+    assert n1; n2; adj1; adj2; adj3 in p1.mad_lib(n1, n2, adj1, adj2, adj3)
+    assert len(p1) == len(p1.journal_entry)
+    p1._User__write_in_journal()
+    test_file = open("journal.txt")
+    test_file_lines = test_file.readlines()
+    assert input("Input previous text.") in test_file_lines
+    test_file.close()
+    print(f"{p1} and all methods in the User class are working successfully.")
+
--- final.py
+++ final.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
-
+"""
+Mariam Ammar
+Class: CS 521 - Summer 2
+Date: 08/20/21
+Final Project 
+A moodbot program that returns options for a user based
+on input relating to their mood. 
+"""
 
 
 # In[ ]:
@@ -15,14 +22,22 @@ import random
 import vlc
 from User import User
 import sys 
+import time
 
 
-#Prompts for user input and reprompts if user input invalid. 
+#Asks user for name and name of friend 
+#assigns default values if blank input is given
 name = input("Please input your first name.")
-close_contact = input("Who's the closest living person to you?")
+if not name:
+    name = "my friend"
+close_contact = input("Who's your best friend?" or "your best friend")
+if not close_contact:
+    close_contact = "your best friend"
 
 print(f"Hi {name}! Welcome to be Moodbot. We have just a few more questions for you.")
 
+#Asks for birthday 
+#Prompts for user input and reprompts if user input invalid. 
 while True:
     year = input('When is your birthday? [YY] ')
     month = input('When is your birthday? [MM] ')
@@ -30,73 +45,92 @@ while True:
 
     try:
         year, month, day = int(year), int(month), int(day)
+        #Convert into datetime format for parsing later 
         birthday = datetime(year,month,day)
         break
 
     except:
-        print("Sorry, try again! Please make sure your number entries follow the right criteria and format.")
-
-    
-while True:
-    mood = input("Rate the way you feel right now from 1-10 with 10 being the best possible score.")
-    try:
-        mood = int(mood)
-        while mood not in range (1,11):
-            mood = int(input("Sorry, try again and make sure you input a number from 1-10.")) 
-        break
-    except:
-        print("Sorry! Looks like you didn't input a number. Please try again.")
-        
-        
-p1 = User(name, close_contact, birthday, mood)
+        print('''Sorry, try again! 
+        Your number entries must follow the right format.''')
 
 
+#This while loop allows user to come back to main menu
+#each time a selection has been made. 
 while True: 
     
-    sad_options = (1, "Listen to a Song"), (2, "Write about it"), (3, "Meditate"),     (4, "Read an inspirational quote"), (5, "Get some tips")
+#Ask user to rate their mood from 1-10
+#Validate input or return error and ask for re-entry
+    while True:
+        mood =         input('''
+        Rate the way you feel right now 
+        from 1-10 with 10 being the best possible score.
+        ''')
 
-    happy_options = (1, "Listen to a Song"), (2, "Write a letter to your future self"),     (3, "Take note of some great ideas"), (4, "Plan your birthday celebration"), (5, "Have a dance party")
+        try:
+            mood = int(mood)
+            while mood not in range (1,11):
+                mood =                 int(input("Sorry, try again and make sure you input a number from 1-10.")) 
+            break
+        except:
+            print("Sorry! Looks like you didn't input a number. Please try again.")
+    
+    #Assign person as p1 to User class 
+    p1 = User(name, close_contact, birthday, mood)
+    
+    #Menu option selections
+    #If users input score 1-5, these selections will be printed
+    sad_options =     (1, "Listen to a Song"),     (2, "Write about it"),     (3, "Meditate"),     (4, "Read an inspirational quote"),     (5, "Get some tips"),    (6, "Read a funny poem")
+    
+    
+    #If user inputs a score of 5 or above, these selections printed
+    happy_options =     (1, "Listen to a Song"),     (2, "Write a letter to your future self"),     (3, "Take note of some great ideas"),     (4, "Plan your birthday celebration"),     (5, "Have a dance party"),    (6, "Read a fun fact")
 
+    #If mood is 1-2, then user is in critical condition
+    #Reminder of closest friend is printed before showing options
     if mood < 3 :
         print(f'''
-        \n Wow. Looks like you're really upset. 
-        Make sure to seek out help and think of how {close_contact}
-        would think if knew you were feeling like this!
+        Wow. Looks like you're really upset. 
+        Make sure to seek out help and think of what {close_contact}
+        would think if they knew you were feeling like this!
         How about giving them a call? 
         Or you can...\n''')
         for i in sad_options:
             print(i[0],i[1])
-
+            
+            
+    #If user input from 3-4, output messsage below with sad options
     if 2 < mood < 5:
         print(f'''
-        \n Why so blue? Your birthday is only {p1.calculate_days_till_bday(p1.birthday)} days away!
-        Prepare a celebration or choose one of the options below.''')
+        Why so blue? 
+Your birthday is only {p1.calculate_days_till_bday(p1.birthday)} days away!
+Prepare a celebration or choose one of the options below.\n''')
         for i in sad_options:
             print(i[0],i[1])
-
+            
+    #If mood score >4 then user is either okay or in good mood. 
+    #Print message with happy options
     if mood > 4:
-        print('''Nice to hear you are at least doing ok!
-    Which option would you like to choose?''')
+        print('''
+        Nice to hear you are at least doing ok!
+        Which option would you like to choose?\n''')
         for i in happy_options:
             print(i[0], i[1])
 
     selection = input("Input a number to select an option.")
     try:       
         selection = int(selection)
-        while selection not in range (1,6):
-            selection = int(input("Sorry, try again and make sure you enter a number according to one of the options.")) 
+        while selection not in range (1,7):
+            selection =             int(input('''Sorry, try again.\n 
+            Enter a number according to one of the options.''')) 
             break
 
     except:
         print("Sorry, try again. You need to input a number.")
 
 
-    dict_happy_options = dict(happy_options)
-    dict_sad_options = dict(sad_options)
-
-
-
-    tips =     {"1. Take a few really deep, controlled breaths.",
+    #Tips to be printed if user selects "Get some tips" options
+    #Could have been also been defined as list of tuple. 
+    tips =     {    "1. Take a few really deep, controlled breaths.",
     "2. Call a good friend.",
     "3. Go for a walk.",
     "4. Do something outside.",
@@ -104,24 +138,43 @@ while True:
     "6. Eat healthy - food is linked to both physical and mental health.",
     "7. Get at least 8 hours of sleep a day.",
     "8. Make time for yourself and include relaxing rituals into your day.",
-    "9. Stay away from smoking and limit your alcohol intake."}
+    "9. Stay away from smoking and limit your alcohol intake."
+    }
 
 
-
-    quotes =     ['"To anyone out there who’s hurting — it’s not a sign of weakness to ask for help. It’s a sign of strength." —Barack Obama',
+    #Inspiration quotes to be printed 
+    #based on user selected opion.  
+    quotes =     [    '''"To anyone out there who’s hurting — 
+     it’s not a sign of weakness to ask for help.\
+    It’s a sign of strength." —Barack Obama''',
     "The only time you fail is when you fall down and stay down.",
     "Every day may not be good... but there’s something good in every day.",
-    '"Soak up the views. Take in the bad weather and the good weather. You are not the storm." —Matt Haig',
-    "A journey of 1000 miles always begins with a single step."]
+    '"Soak up the views. Take in the bad weather and the good weather. \
+    You are not the storm." —Matt Haig',
+    "A journey of 1000 miles always begins with a single step."
+    ]
 
+    #fun facts to be printed based on option
+    fun_facts =     ("More people get attacked every year by a cow than by a shark.",
+     "When you cut a hole in your fishing net, it has fewer holes.",
+     "Pennsylvania, USA, has a law which bans you from sleeping on a fridge.",
+     "Hot water will turn into ice faster than cold water.",
+     "The Mona Lisa has no eyebrows",
+     "The entire world's population could fit inside Los Angeles.")
 
-    p = vlc.MediaPlayer("theclimb.mp3")
-    sad_song = "theclimb.mp3"
-    happy_song = "toosieslide.mp3"
-    meditation = "meditation.mp3"
-    dance_party = "dance_party.mp3"
+    #Songs to be play based on user selected option and mood. 
+    dict_audio = {"sad_song" : "theclimb.mp3", 
+              "happy_song" : "toosieslide.mp3",
+              "meditation" : "meditation.mp3" , 
+              "dance_party" : "dance_party.mp3"}
+    
+
 
     def play_audio(filename):   
+        '''
+        Takes filename as argument and plays song file
+        Allows user to stop song by pressins 's
+        '''
         p = vlc.MediaPlayer(filename)
         p.play()
         while True:
@@ -131,39 +184,102 @@ while True:
                 break
             else:
                 print("Sorry, you can only press 's' to stop the audio.")
-
+                
+    #Output options based on user input if unhappy 
     if mood < 5 and selection == 1:
-        play_audio(sad_song)
+        play_audio(dict_audio["sad_song"])
     elif mood < 5 and selection == 2:
-        p1._User__write_in_journal()
+    #Calls method from User class for user to write in journal
+        print(p1._User__write_in_journal())
     elif mood < 5 and selection == 3:
-        play_audio(meditation)
+    #Plays meditation audio
+        play_audio(dict_audio["meditation"])
     elif mood < 5 and selection == 4:
-        index = random.randrange(0, 5)
-        print(quotes[index-1])
+        #Prints quote at random inded from quotes list
+        index = random.randrange(0,len(quotes)-1)
+        print(quotes[index])
     elif mood < 5 and selection == 5:
+        #Prints tips for long-term mental/physical health
         for i in sorted(tips):
             print(i)
+    elif mood < 5 and selection == 6:
+        #Asks for user words to input 
+        #Users inputs as arguments to generate madlib from User class
+        while True:
+            try:
+                n1, n2 =                 input("Input two nouns with a space in between.").split()
+                adj1, adj2, adj3                = input("Input three adjectives with a space in between.")                .split()
+            except:
+                print("Sorry try again.")
+            else:
+                print(p1.mad_lib(n1, n2, adj1, adj2, adj3)+"\n")
+                break
 
     if mood > 4 and selection == 1:
-        play_audio(happy_song)
+        #Plays "Happy" song
+        play_audio(dict_audio["happy_song"])
+    #Next three options allow user to input entry into journal
     elif mood > 4 and selection == 2:
-        p1._User__write_in_journal()
+        print(p1._User__write_in_journal())
     elif mood > 4 and selection == 3:
-        p1._User__write_in_journal()
+        print(p1._User__write_in_journal())
+    #Next three options allow user to input entry into journal
     elif mood > 4 and selection == 4:
-        print(f'''Wow! Only {p1.calculate_days_till_bday(p1.birthday)} till your birthday.
+        #Print statement with days till birthday calculated
+        #Then allow for journal entry
+        print(f'''Wow! 
+        Only {p1.calculate_days_till_bday(p1.birthday)} till your birthday.
         How will you celebrate and who will you invite?''')
-        p1._User__write_in_journal()
+        print(p1._User__write_in_journal())
     elif mood > 4 and selection == 5:
-        play_audio(dance_party)
+        #Prints funny string character by character using time delay
+        strng = "~~~~~~~~~!!!!! ♪┏(・o･)┛♪┗ ( ･o･) ┓♪~~~~~~!!!!~~~~~~"
+        for i in strng:
+            print(i, end = "")
+            time.sleep(0.05)
+        #Play dance party audio as soon as string finishes printing
+        play_audio(dict_audio["dance_party"])
 
-    user_input = input("Press 'b' to go back to the main menu or another character to exit the program.")
+    
+    elif mood > 4 and selection == 6:
+        index = random.randrange(0, len(fun_facts)-1)
+        print("\n"+fun_facts[index])
+
+    user_input =     input    ("\nPress 'b' to go back to the main menu or another character to exit the program.")
 
     if user_input == 'b':
         continue
     else:
         sys.exit(0)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+strng = "aslkdfjdas;lkjfa"
+
+for l in strng*50:
+    for i in strng:
+        print(i, end = "")
+        time.sleep(0.04)
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
